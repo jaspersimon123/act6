1e56b3ac1d1de35b78f754e81dd3bbf816ba3d81
jaspersimon123 <occ.ebrado.jaspersimon@gmail.com>
2023-10-17 11:52:40 +0800

    first commit

---
+++ app/page.js
@@ -0,0 +1,27 @@
+'use client'
+import React, {useState } from 'react';
+export default function Home() {
+  const [number, setNum] = useState(0);
+
+  const increment = () => {
+    setNum((prevNum) => prevNum + 1); 
+  }
+    
+  const decrement = () => {
+    setNum((prevNum) => prevNum - 1);
+  }
+
+  return (
+    <center>
+    <div>
+      <span><h1>COUNTER</h1></span>
+      <span>counter: {number}</span>
+
+      <div className='bContainer'>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+      </div>
+    </div>
+    </center>
+  )
+}
